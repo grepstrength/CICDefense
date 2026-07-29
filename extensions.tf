4b1e0f5c2ac6121bc0ae24ddae057c5bd712df51
grepstrength <admin@grepstrength.dev>
2026-07-28 21:56:08 -0400

    Merge Windows scripts + switch extension to fileUris

--- extensions.tf
+++ extensions.tf
@@ -1,47 +1,18 @@
-resource "azurerm_virtual_machine_extension" "windows_base" {
-  name               = "install-base"
-  virtual_machine_id = azurerm_windows_virtual_machine.windows.id
-  // implicit dependency that guarantees the VM exists before the extension runs
-  publisher                  = "Microsoft.Compute"
-  type                       = "CustomScriptExtension"
-  type_handler_version       = "1.10"
-  auto_upgrade_minor_version = true
-  tags                       = var.tags
-
-  protected_settings = jsonencode({
-    commandToExecute = join("", [
-      "powershell -ExecutionPolicy Unrestricted -EncodedCommand ",
-      textencodebase64(
-        file("${path.module}/scripts/windows-base-setup.ps1"),
-        "UTF-16LE"
-      )
-    ])
-  })
-
-  timeouts {
-    create = "60m" // 60 minute timeout - overrides the default timeout and gives plenty of headroom 
-  }
-}
-
-resource "azurerm_virtual_machine_extension" "windows_detection" {
-  name                       = "install-detection"
+resource "azurerm_virtual_machine_extension" "windows" {
+  name                       = "install-tooling"
   virtual_machine_id         = azurerm_windows_virtual_machine.windows.id
   publisher                  = "Microsoft.Compute"
   type                       = "CustomScriptExtension"
   type_handler_version       = "1.10"
   auto_upgrade_minor_version = true
   tags                       = var.tags
-  protected_settings = jsonencode({
-    commandToExecute = join("", [
-      "powershell -ExecutionPolicy Unrestricted -EncodedCommand ",
-      textencodebase64(
-        file("${path.module}/scripts/windows-detection-setup.ps1"),
-        "UTF-16LE"
-      )
-    ])
-  })
 
-  provision_after_extensions = ["install-base"] // this forces the detection extension to wait for the base to finsih 
+  settings = jsonencode({
+    fileUris = [
+      "https://raw.githubusercontent.com/grepstrength/CICDefense/main/scripts/windows-setup.ps1"
+    ]
+    commandToExecute = "powershell -ExecutionPolicy Unrestricted -File windows-setup.ps1"
+  })
 
   timeouts {
     create = "60m"
